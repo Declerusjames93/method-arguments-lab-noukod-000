4ec18c815f726ad48e1e76f4362f7ccb9282b7f4
James Declerus <james.declerus@esih.edu>
2019-04-11 15:51:40 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name, language)
   name = "Josh"
   language = "Ruby"
-  puts "Hi, my name is #{name}."
+  puts `"Hi, my name is #{name} and I am learning to program in #{language}."
 end
